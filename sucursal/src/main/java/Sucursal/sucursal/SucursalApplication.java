4863166cf08c46607a5711043320a46173418e08
MariFantini707 <ma.fantini@duocuc.cl>
2025-05-23 12:51:23 -0400

    coloqué los rest template en sucursal

--- sucursal/src/main/java/Sucursal/sucursal/SucursalApplication.java
+++ sucursal/src/main/java/Sucursal/sucursal/SucursalApplication.java
@@ -2,6 +2,8 @@ package Sucursal.sucursal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SucursalApplication {
@@ -9,5 +11,11 @@ public class SucursalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SucursalApplication.class, args);
 	}
+
+	@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+
 
 }
