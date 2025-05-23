195aa39b9f0bd7d9fc79ab51f221afe731a12988
Salomorwil <salomorwil@gmail.com>
2025-05-23 11:24:59 -0400

    Quitar getters y seters por lombok

--- sucursal/src/main/java/Sucursal/sucursal/Modelo/Producto.java
+++ sucursal/src/main/java/Sucursal/sucursal/Modelo/Producto.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "producto")
+@Data
 public class Producto {
 
     @Id
@@ -24,13 +26,4 @@ public class Producto {
     @Column(name = "categoriaProducto", nullable = false, length = 50)
     private String categoriaProducto;
 
-    // Getters y setters
-    public Integer getIdProducto() { return idProducto; }
-    public void setIdProducto(Integer idProducto) { this.idProducto = idProducto; }
-    public String getNombreProducto() { return nombreProducto; }
-    public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
-    public Integer getPrecioProducto() { return precioProducto; }
-    public void setPrecioProducto(Integer precioProducto) { this.precioProducto = precioProducto; }
-    public String getCategoriaProducto() { return categoriaProducto; }
-    public void setCategoriaProducto(String categoriaProducto) { this.categoriaProducto = categoriaProducto; }
 }
--- sucursal/src/main/java/Sucursal/sucursal/Modelo/Stock.java
+++ sucursal/src/main/java/Sucursal/sucursal/Modelo/Stock.java
@@ -1,5 +1,25 @@
 package Sucursal.sucursal.Modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "stock")
+@Data
 public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idStock;
+
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "idProducto", nullable = false)
+    private Integer idProducto;
 
 }
